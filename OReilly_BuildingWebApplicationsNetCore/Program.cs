105759284e3dd5df234357b5e40214d2b15a1e20
seangnewman <seangnewman@msn.com>
2021-04-10 03:13:47 -0700

    Add project files.

---
+++ OReilly_BuildingWebApplicationsNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OReilly_BuildingWebApplicationsNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SpyStore.DAL/EF/MyConnectionStrategy.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+
+namespace SpyStore.DAL.EF
+{
+    class MyConnectionStrategy : ExecutionStrategy
+    {
+        public MyConnectionStrategy(ExecutionStrategyDependencies context) :
+            base(context, ExecutionStrategy.DefaultMaxRetryCount, ExecutionStrategy.DefaultMaxDelay)
+        {
+
+        }
+
+        public MyConnectionStrategy(ExecutionStrategyDependencies context, int maxRetryCount, TimeSpan maxRetryDelay) :
+            base(context, maxRetryCount, maxRetryDelay)
+        {
+
+        }
+
+        protected override bool ShouldRetryOn(Exception exception)
+        {
+            return true;
+        }
+    }
+}
