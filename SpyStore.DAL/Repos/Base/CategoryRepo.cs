60e398a5b4180d2e7e4a0b99ea81982356d9e582
seangnewman <seangnewman@msn.com>
2021-04-11 00:04:35 -0700

    Completion of the  repositories for Category and Base

---
+++ SpyStore.DAL/Repos/Base/CategoryRepo.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpyStore.DAL.EF;
+using SpyStore.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpyStore.DAL.Repos.Base
+{
+    class CategoryRepo:RepoBase<Category>
+    {
+        public CategoryRepo(DbContextOptions<StoreContext> options): base(options)
+        {
+
+        }
+
+        public CategoryRepo()
+        {
+
+        }
+
+        public override IEnumerable<Category> GetAll() => Table.OrderBy(x => x.CategoryName);
+        public override IEnumerable<Category> GetRange(int skip, int take) => GetRange(Table.OrderBy(x => x.CategoryName), skip, take);
+    }
+}
